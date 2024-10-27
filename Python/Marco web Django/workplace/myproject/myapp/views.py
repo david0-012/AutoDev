bfa487512be5ededb04beff12f2d889347ef8e7d
Juan David Casallas Pava <davidcasallas1202@gmail.com>
2024-10-27 00:01:20 -0500

    Mapeo de URL con parámetros

--- Python/Marco web Django/workplace/myproject/myapp/views.py
+++ Python/Marco web Django/workplace/myproject/myapp/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1> Welcome to Little Lemon! </h1>")
+
+def drinks(request, drink_name):
+    drink = {
+        'mocha' : 'type of coffee',
+        'tea' : 'type of beverage',
+        'lemonade' : 'type of refreshment',
+    }
+    choice_of_drink = drink.get(drink_name)
+    return HttpResponse(f"<h2> {drink_name} </h2>" + choice_of_drink)
