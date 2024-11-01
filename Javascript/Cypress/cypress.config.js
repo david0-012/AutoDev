f62bc6d1f66c3f50dabc44036fd3b9064d9cd5ec
Juan David Casallas Pava <davidcasallas1202@gmail.com>
2024-10-31 22:06:34 -0500

    Configuración para Html reports

--- Javascript/Cypress/cypress.config.js
+++ Javascript/Cypress/cypress.config.js
@@ -1,9 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    defaultCommandTimeout: 10000,
+    video: true, // Habilitar grabación de video
+    screenshotOnRunFailure: true, // Habilitar capturas de pantalla en caso de fallo
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
